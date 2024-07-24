458965d68ff415a725d820ac7d24b4cd5c8b4145
viacharles <viacharles@gmail.com>
2024-07-24 18:10:37 +0800

    新增 layout 元件

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
+import { LayoutComponent } from './modules/layout/layout.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
