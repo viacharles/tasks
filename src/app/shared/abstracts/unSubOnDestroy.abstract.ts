ed5accb25cb758e7a21a0e5bac41f6a0df0f73c2
viacharles <viacharles@gmail.com>
2024-07-24 18:23:51 +0800

    新增 onDestory 抽象繼承

---
+++ src/app/shared/abstracts/unSubOnDestroy.abstract.ts
@@ -0,0 +1,18 @@
+import { Injectable, OnDestroy } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Injectable()
+export abstract class UnSubOnDestroy implements OnDestroy {
+  private onDestroySubject = new Subject<void>();
+  public onDestroy$ = this.onDestroySubject.asObservable();
+
+  ngOnDestroy(): void {
+    this.basePageOnDestroy();
+    this.onDestroySubject.next();
+    this.onDestroySubject.complete();
+  }
+  /** base 頁面專用 onDestroy */
+  protected basePageOnDestroy(): void { }
+  /** 最外層繼承使用 */
+  protected onDestroy(): void { }
+}
