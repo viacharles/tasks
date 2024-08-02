234280992aa65bfbe593337f545cf277946bfe62
viacharles <viacharles@gmail.com>
2024-08-02 23:36:19 +0800

    測試環境設定

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+
+export const environment = {
+  production: true,
+  baseHref: "https://viacharles.github.io/tasks/"
+};
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,27 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
+// if (environment.production) {
+//   enableProdMode();
+// }
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const base = document.querySelector('base');
+//   if (!!base) {
+//     const currentHref = base.getAttribute('href');
+//     const targetHref = environment.baseHref;
+//     if (currentHref !== targetHref) {
+//       base.setAttribute('href', targetHref);
+//       location.replace(targetHref);
+//     } else {
+//       bootstrapApplication(AppComponent, appConfig)
+//         .catch((err) => console.error(err));
+//     };
+//   } else {
+//     bootstrapApplication(AppComponent, appConfig)
+//       .catch((err) => console.error(err));
+//   };
+// })
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
