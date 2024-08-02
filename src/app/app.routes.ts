a8fcf3c4818aaed34dcdb6a697c6e96542a9adf0
viacharles <viacharles@gmail.com>
2024-08-02 10:24:24 +0800

    app: route setting

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TaskPageComponent } from './modules/task/task.page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'task', pathMatch: 'full' },
+  { path: 'task', component: TaskPageComponent },
+  { path: '**', redirectTo: 'task', pathMatch: 'full' },
+];
