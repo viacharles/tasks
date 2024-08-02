f1920957b267d3efc17e2d4edf81aa77c6378397
viacharles <viacharles@gmail.com>
2024-08-03 01:31:31 +0800

    fix: environment setting

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
 
-// if (environment.production) {
-//   enableProdMode();
-// }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const base = document.querySelector('base');
-//   if (!!base) {
-//     const currentHref = base.getAttribute('href');
-//     const targetHref = environment.baseHref;
-//     if (currentHref !== targetHref) {
-//       base.setAttribute('href', targetHref);
-//       location.replace(targetHref);
-//     } else {
-//       bootstrapApplication(AppComponent, appConfig)
-//         .catch((err) => console.error(err));
-//     };
-//   } else {
-//     bootstrapApplication(AppComponent, appConfig)
-//       .catch((err) => console.error(err));
-//   };
-// })
+if (environment.production) {
+  enableProdMode();
+}
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
